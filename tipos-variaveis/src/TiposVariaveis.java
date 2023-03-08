9cd598f048df675c3657a87b695b3ab63f1c76ab
Matheus <mathdk95@gmail.com>
2023-03-08 17:22:20 -0300

    acrescentando uso de constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,7 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+
+        // tipos primitivos
+        //classe String que representa texto na aplicação
+
+        String meuNome = "MATHEUS";
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;       
     }
 }
